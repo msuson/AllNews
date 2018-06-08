26d5e320a8526f6108d40dcb22be345563d71f0e
Marco Suson <masuson117@gmail.com>
2018-06-07 22:17:13 -0700

    Initial commit

---
+++ app/src/main/java/com/example/marco/allnews/NewsFeedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.marco.allnews
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsFeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_feed)
+    }
+}
